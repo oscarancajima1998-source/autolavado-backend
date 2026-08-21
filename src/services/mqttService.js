11d93da0af645d5a0488d966a029c18a570982e3
carwashpagosyape-stack <carwash.pagos.yape@gmail.com>
2026-08-21 15:12:37 -0500

    mqqt

--- src/services/mqttService.js
+++ src/services/mqttService.js
@@ -1,17 +1,19 @@
-const mqtt = require('mqtt');
+const mqtt = require('require');
 require('dotenv').config();
 
-// 1. Detecta si hay una URL en la nube (Render) o usa tu Mosquitto local
 const mqttUrl = process.env.MQTT_HOST || 'mqtt://localhost:1883';
 const opciones = {};
 
-// 2. Si detecta que creaste un usuario en Render, se lo agrega a la conexión
+// --- MODO ESPÍA ACTIVADO ---
+console.log('🔍 [DEBUG] Intentando conectar a:', mqttUrl);
+console.log('🔍 [DEBUG] Usuario configurado:', process.env.MQTT_USER ? process.env.MQTT_USER : 'NINGUNO (¡Aquí está el error!)');
+// ---------------------------
+
 if (process.env.MQTT_USER) {
   opciones.username = process.env.MQTT_USER;
   opciones.password = process.env.MQTT_PASS;
 }
 
-// 3. Intenta conectarse usando la URL y las opciones (si las hay)
 const client = mqtt.connect(mqttUrl, opciones);
 
 client.on('connect', () => {
