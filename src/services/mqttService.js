182c8aa94c75c077dc0862ace2553e490a05d1c7
carwashpagosyape-stack <carwash.pagos.yape@gmail.com>
2026-08-21 15:10:41 -0500

    Modo espia MQTT

--- src/services/mqttService.js
+++ src/services/mqttService.js
@@ -1,10 +1,21 @@
 const mqtt = require('mqtt');
 require('dotenv').config();
 
-const client = mqtt.connect(process.env.MQTT_HOST || 'mqtt://localhost');
+// 1. Detecta si hay una URL en la nube (Render) o usa tu Mosquitto local
+const mqttUrl = process.env.MQTT_HOST || 'mqtt://localhost:1883';
+const opciones = {};
+
+// 2. Si detecta que creaste un usuario en Render, se lo agrega a la conexión
+if (process.env.MQTT_USER) {
+  opciones.username = process.env.MQTT_USER;
+  opciones.password = process.env.MQTT_PASS;
+}
+
+// 3. Intenta conectarse usando la URL y las opciones (si las hay)
+const client = mqtt.connect(mqttUrl, opciones);
 
 client.on('connect', () => {
-  console.log('📡 Conectado exitosamente al Broker MQTT (Mosquitto)');
+  console.log('📡 Conectado exitosamente al Broker MQTT');
 });
 
 client.on('error', (err) => {
